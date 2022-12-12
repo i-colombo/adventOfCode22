6bc100d3cc3070b3c654e4a273c9cea56235da76
Ignacio Colombo <icolombo.91@gmail.com>
2022-12-12 20:41:50 -0300

    Adding Day 5 part 1

---
+++ Day_5/main.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from stacks import *
+
+path = Path(__file__).with_name('input.txt')
+with path.open() as file:
+    stacks = initializeStacks()
+    for line in file:
+        movements = getNumberOfMovements(line)
+        stack_from = getStackFrom(line)
+        stack_to = getStackTo(line)
+
+        moveFromTo(movements, stacks[stack_from], stacks[stack_to])
+    
+    all_top_crates = ""
+    for stack in stacks:
+        top_crate = stacks[stack].pop()
+        all_top_crates += top_crate
+        print(f"Part 1 :: The top crate of stack: {stack} is: {top_crate}")
+    print(f"Part 1 :: All top crates: {all_top_crates}")
---
+++ Day_5/stacks.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+def initializeStacks():
+    return dict([
+        (1, deque(['S', "Z", "P", "D", "L", "B", "F", "C"])),
+        (2, deque(["N", "V", "G", "P", "H", "W", "B"])),
+        (3, deque(["F", "W", "B", "J", "G"])),
+        (4, deque(["G", "J", "N", "F", "L", "W", "C", "S"])),
+        (5, deque(["W", "J", "L", "T", "P", "M", "S", "H"])),
+        (6, deque(["B", "C", "W", "G", "F", "S"])),
+        (7, deque(["H", "T", "P", "M", "Q", "B", "W"])),
+        (8, deque(["F", "S", "W", "T"])),
+        (9, deque(["N", "C", "R"]))
+    ])
+
+def initializeStacksForTest():
+    return dict([
+        (1, deque(['Z', "N"])),
+        (2, deque(["M", "C", "D"])),
+        (3, deque(["P"]))
+    ])
+
+def getNumberOfMovements(line: str) -> int:
+    # eg: "move 2 from 5 to 9", returns 2
+    return int(line.split(" ")[1])
+
+def getStackFrom(line: str) -> int:
+    # eg: "move 2 from 5 to 9", returns 5
+    return int(line.split(" ")[3])
+
+def getStackTo(line: str) -> int:
+    # eg: "move 2 from 5 to 9", returns 9
+    return int(line.split(" ")[5])
+
+def moveFromTo(movements: int, stack_from: deque, stack_to: deque):
+    for _ in range(movements):
+        value = stack_from.pop()
+        stack_to.append(value)
