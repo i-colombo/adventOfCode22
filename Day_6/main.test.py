caecd90fbe0dca63f4db399ec5bc073e09c65a04
Ignacio Colombo <icolombo.91@gmail.com>
2022-12-14 18:42:02 -0300

    Adding Day 6

---
+++ Day_6/main.test.py
@@ -0,0 +1,6 @@
+from main import *
+
+assert startOfPacketMarker("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
+assert startOfPacketMarker("nppdvjthqldpwncqszvftbrmjlhg") == 6
+assert startOfPacketMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
+assert startOfPacketMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
