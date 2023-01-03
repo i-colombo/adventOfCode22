4b4f7250bcf5ee51bf93b9a299fb0d3572daead3
Ignacio Colombo <icolombo.91@gmail.com>
2023-01-03 15:21:29 -0300

    Day 11 - Completed part 1

--- Day_11/main.py
+++ Day_11/main.py
@@ -1,15 +1,13 @@
 from pathlib import Path
 from monkeys import MonkeyBuilder
+from monkeys import Monkey
 
 path = Path(__file__).with_name('input.txt')
-monkeys = []
 current_monkey_builder = None
 with path.open() as file:
     for line in file:
         instruction = line.strip()
-        if instruction[:6] == "Monkey":
-            if current_monkey_builder != None:
-                monkeys.append(current_monkey_builder.build())
+        if instruction[:6] == "Monkey":        
             current_monkey_builder = MonkeyBuilder(name = instruction[7:])
         elif instruction[:15] == "Starting items:":
             items = [int(item) for item in instruction[15:].split(",")]
@@ -26,6 +24,18 @@ with path.open() as file:
         elif instruction[:9] == "If false:":
             calmed_target_monkey = int(instruction[26:])
             current_monkey_builder.setCalmedTargetMonkey(calmed_target_monkey)
+            current_monkey_builder.build()
 
-[print(monkey) for monkey in monkeys]
+number_of_rounds = 20
+for _ in range(number_of_rounds):
+    for monkey in Monkey.monkeys:
+        monkey.throwItems()
 
+[print(monkey) for monkey in Monkey.monkeys]
+
+inspected_counts = [monkey.getInspectedItemsCount() for monkey in Monkey.monkeys]
+inspected_counts.sort(reverse=True)
+print(f"Part 1 :: Inspected counts: {inspected_counts}")
+
+monkey_business = inspected_counts[0] * inspected_counts[1]
+print(f"Part 1 :: Monkey business: {monkey_business}")
--- Day_11/monkeys.py
+++ Day_11/monkeys.py
@@ -1,4 +1,6 @@
 class Monkey():
+    monkeys = []
+
     def __init__(self, name: str, items: list, worry_operation: str, test_worry_operation: int, worried_target_monkey: int, calmed_target_monkey: int):
         self.__name = name
         self.__items = items
@@ -6,9 +8,61 @@ class Monkey():
         self.__test_worry_operation = test_worry_operation
         self.__worried_target_monkey = worried_target_monkey
         self.__calmed_target_monkey = calmed_target_monkey
+        self.__inspected_items_count = 0
+        Monkey.monkeys.append(self)
 
     def __repr__(self):
-        return f'Name: {self.__name} \n- Items: {self.__items} \n- Worry Op: {self.__worry_operation} \n- Test Op: {self.__test_worry_operation} \n- Worried Monkey: {self.__worried_target_monkey} \n- Calmed Monkey: {self.__calmed_target_monkey}'
+        return f'Name: {self.__name} \n- Items: {self.__items} \n- Worry Op: {self.__worry_operation} \n- Test Op: {self.__test_worry_operation} \n- Worried Monkey: {self.__worried_target_monkey} \n- Calmed Monkey: {self.__calmed_target_monkey} \n- Inspected items count: {self.__inspected_items_count}'
+
+    def throwItems(self):
+        self.__inspected_items_count += len(self.__items)
+        for _ in range(len(self.__items)):
+            item = self.__items.pop(0)
+            new_worry_level = self.boringItem(self.humanWorryOfItem(item))
+            self.throwItem(new_worry_level)
+    
+    def humanWorryOfItem(self, item: int) -> int:
+        operation = self.__worry_operation.split(" ")
+        operator = operation[0]
+        term = operation[1]
+        if term == "old":
+            return self.calculate(operator, item, item)
+        else:
+            return self.calculate(operator, item, int(term))
+
+    def boringItem(self, item_worry: int) -> int:
+        return self.calculate("/", item_worry, 3)
+
+    def calculate(self, operator: str, left_term: int, right_term: int) -> int:
+        if operator == "+":
+            return left_term + right_term
+        elif operator == "-":
+            return left_term - right_term
+        elif operator == "*":
+            return left_term * right_term
+        elif operator == "/":
+            return int(left_term / right_term)
+        else:
+            raise Exception("Unknown operator")
+    
+    def throwItem(self, item: int):
+        if self.isHumanWorried(item):
+            self.thrownItemToMonkey(item, self.__worried_target_monkey)
+        else:
+            self.thrownItemToMonkey(item, self.__calmed_target_monkey)
+    
+    def isHumanWorried(self, item: int) -> bool:
+        return item % self.__test_worry_operation == 0
+
+    def thrownItemToMonkey(self, item: int, target_monkey_order: int):
+        target_monkey = Monkey.monkeys[target_monkey_order]
+        target_monkey.receive(item)
+
+    def receive(self, item: int):
+        self.__items.append(item)
+
+    def getInspectedItemsCount(self) -> int:
+        return self.__inspected_items_count
 
 class MonkeyBuilder():
     def __init__(self, name: str = "", items: list = [], worry_operation: str = "", test_worry_operation: int = 1, worried_target_monkey: int = 0, calmed_target_monkey: int = 0):
