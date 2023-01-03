a8e9187e46708faa1d9430183082c48bdcf5436e
Ignacio Colombo <icolombo.91@gmail.com>
2023-01-03 12:23:36 -0300

    Day 11 Part 1 - refactoring monkey creation

--- Day_11/main.py
+++ Day_11/main.py
@@ -1,30 +1,31 @@
 from pathlib import Path
-from monkeys import Monkey
+from monkeys import MonkeyBuilder
 
 path = Path(__file__).with_name('input.txt')
 monkeys = []
-current_monkey = None
+current_monkey_builder = None
 with path.open() as file:
     for line in file:
         instruction = line.strip()
         if instruction[:6] == "Monkey":
-            current_monkey = Monkey(name = instruction[7:])
-            monkeys.append(current_monkey)
+            if current_monkey_builder != None:
+                monkeys.append(current_monkey_builder.build())
+            current_monkey_builder = MonkeyBuilder(name = instruction[7:])
         elif instruction[:15] == "Starting items:":
             items = [int(item) for item in instruction[15:].split(",")]
-            current_monkey.setItems(items)
+            current_monkey_builder.setItems(items)
         elif instruction[:10] == "Operation:":
             operation = instruction[21:].strip()
-            current_monkey.setWorryOperation(operation)
+            current_monkey_builder.setWorryOperation(operation)
         elif instruction[:5] == "Test:":
             test_operation = int(instruction[19:].strip())
-            current_monkey.setTestWorryOperation(test_operation)
+            current_monkey_builder.setTestWorryOperation(test_operation)
         elif instruction[:8] == "If true:":
             worried_target_monkey = int(instruction[25:])
-            current_monkey.setWorriedTargetMonkey(worried_target_monkey)
+            current_monkey_builder.setWorriedTargetMonkey(worried_target_monkey)
         elif instruction[:9] == "If false:":
             calmed_target_monkey = int(instruction[26:])
-            current_monkey.setCalmedTargetMonkey(calmed_target_monkey)
+            current_monkey_builder.setCalmedTargetMonkey(calmed_target_monkey)
 
 [print(monkey) for monkey in monkeys]
 
--- Day_11/monkeys.py
+++ Day_11/monkeys.py
@@ -1,5 +1,5 @@
 class Monkey():
-    def __init__(self, name: str = "", items: list = [], worry_operation: str = "", test_worry_operation: int = 1, worried_target_monkey: int = 0, calmed_target_monkey: int = 0):
+    def __init__(self, name: str, items: list, worry_operation: str, test_worry_operation: int, worried_target_monkey: int, calmed_target_monkey: int):
         self.__name = name
         self.__items = items
         self.__worry_operation = worry_operation
@@ -9,7 +9,16 @@ class Monkey():
 
     def __repr__(self):
         return f'Name: {self.__name} \n- Items: {self.__items} \n- Worry Op: {self.__worry_operation} \n- Test Op: {self.__test_worry_operation} \n- Worried Monkey: {self.__worried_target_monkey} \n- Calmed Monkey: {self.__calmed_target_monkey}'
-    
+
+class MonkeyBuilder():
+    def __init__(self, name: str = "", items: list = [], worry_operation: str = "", test_worry_operation: int = 1, worried_target_monkey: int = 0, calmed_target_monkey: int = 0):
+        self.__name = name
+        self.__items = items
+        self.__worry_operation = worry_operation
+        self.__test_worry_operation = test_worry_operation
+        self.__worried_target_monkey = worried_target_monkey
+        self.__calmed_target_monkey = calmed_target_monkey
+
     def setItems(self, items: list):
         self.__items = items
     
@@ -24,3 +33,6 @@ class Monkey():
 
     def setCalmedTargetMonkey(self, calmed_target_monkey: int):
         self.__calmed_target_monkey = calmed_target_monkey
+
+    def build(self) -> Monkey:
+        return Monkey(self.__name, self.__items, self.__worry_operation, self.__test_worry_operation, self.__worried_target_monkey, self.__calmed_target_monkey)
