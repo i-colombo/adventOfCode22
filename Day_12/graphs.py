0d3bb52643d96d4c23893978d2f039a9341a6853
Ignacio Colombo <icolombo.91@gmail.com>
2023-01-16 10:45:44 -0300

    Day 12 : improving part one

--- Day_12/graphs.py
+++ Day_12/graphs.py
@@ -1,12 +1,12 @@
 from dijkstra import Graph
 
-def minimumDistancesTo(height_map: list, start_index: int) -> list:
-    vertices_num = len(height_map) * len(height_map[0])
+def minimumDistancesStartingFrom(adjacency_map: list, start_index: int) -> list:
+    vertices_num = len(adjacency_map)
     g = Graph(vertices_num)
-    g.graph = adjacencyMapFromHeightMap(height_map)
+    g.graph = adjacency_map
     return g.dijkstra(start_index)
 
-def adjacencyMapFromHeightMap(height_map: list) -> list:
+def adjacencyMapFromHeightMap(height_map: list, calculateConnectionWeight: 'function') -> list:
     vertices_count = len(height_map) * len(height_map[0])
     adjacency_map = []
     for i, map_line in enumerate(height_map):
@@ -15,21 +15,19 @@ def adjacencyMapFromHeightMap(height_map: list) -> list:
             vertex_connections = [0] * vertices_count
             if i > 0:
                 connection_height = height_map[i-1][j]
-                computeVertexConnection(current_map_height, connection_height, vertex_connections, i-1, j, vertices_line_num)
+                connection_adjacency_index = (i-1) * vertices_line_num + j
+                vertex_connections[connection_adjacency_index] = calculateConnectionWeight(current_map_height, connection_height)
             if j > 0:
                 connection_height = height_map[i][j-1]
-                computeVertexConnection(current_map_height, connection_height, vertex_connections, i, j-1, vertices_line_num)
+                connection_adjacency_index = i * vertices_line_num + j-1
+                vertex_connections[connection_adjacency_index] = calculateConnectionWeight(current_map_height, connection_height)
             if i < len(height_map) - 1:
                 connection_height = height_map[i+1][j]
-                computeVertexConnection(current_map_height, connection_height, vertex_connections, i+1, j, vertices_line_num)
+                connection_adjacency_index = (i + 1) * vertices_line_num + j
+                vertex_connections[connection_adjacency_index] = calculateConnectionWeight(current_map_height, connection_height)
             if j < len(map_line) - 1:
                 connection_height = height_map[i][j+1]
-                computeVertexConnection(current_map_height, connection_height, vertex_connections, i, j+1, vertices_line_num)
+                connection_adjacency_index = i * vertices_line_num + j+1
+                vertex_connections[connection_adjacency_index] = calculateConnectionWeight(current_map_height, connection_height)
             adjacency_map.append(vertex_connections)
     return adjacency_map
-
-
-def computeVertexConnection(current_height, connection_height, connections, connection_i, connection_j, map_width):
-    if connection_height - current_height <= 1:
-        connection_adjacency_index = connection_i * map_width + connection_j
-        connections[connection_adjacency_index] = 1
--- Day_12/main.py
+++ Day_12/main.py
@@ -1,10 +1,17 @@
 from pathlib import Path
-from graphs import minimumDistancesTo
+from graphs import *
+
+def uphillConnectionWeightCalculator(current_height: int, connection_height: int) -> int:
+    if connection_height - current_height <= 1:
+        return 1
+    else:
+        return 0
 
 path = Path(__file__).with_name('input.txt')
 height_map = []
 start_vertex = 0
 finish_vertex = 0
+lower_elevation_indexes = []
 with path.open() as file:
     map_str = [line.strip() for line in file.readlines()]
     for i, map_line in enumerate(map_str):
@@ -13,12 +20,16 @@ with path.open() as file:
             if map_item == 'S':
                 height_map[i][j] = ord("a") - 97
                 start_vertex = i * len(map_line) + j
+                lower_elevation_indexes.append(start_vertex)
             elif map_item == 'E':
                 height_map[i][j] = ord("z") - 97
                 finish_vertex = i * len(map_line) + j
             else:
                 height_map[i][j] = ord(map_item) - 97
+                if map_item == 'a':
+                    lower_elevation_indexes.append(i * len(map_line) + j)
 
-min_distances = minimumDistancesTo(height_map, start_vertex)
+adjacency_map = adjacencyMapFromHeightMap(height_map, uphillConnectionWeightCalculator)
+min_distances = minimumDistancesStartingFrom(adjacency_map, start_vertex)
 
 print(f"Part 1 :: Minimum steps to top: {min_distances[finish_vertex]}" )
